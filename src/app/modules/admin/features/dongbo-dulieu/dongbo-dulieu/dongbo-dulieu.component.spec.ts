902c13b86adca28a5020fa8f944b002efb8bedce
Phuong65 <145210443+Phuong65@users.noreply.github.com>
2026-09-25 14:50:00 +0700

    aa

--- src/app/modules/admin/features/dongbo-dulieu/dongbo-dulieu/dongbo-dulieu.component.spec.ts
+++ src/app/modules/admin/features/dongbo-dulieu/dongbo-dulieu/dongbo-dulieu.component.spec.ts
@@ -1,3 +1,27 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DongboDulieuComponent } from './dongbo-dulieu.component';
+
+describe('DongboDulieuComponent', () => {
+  let component: DongboDulieuComponent;
+  let fixture: ComponentFixture<DongboDulieuComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ DongboDulieuComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(DongboDulieuComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
+
 <<<<<<< HEAD
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 =======
